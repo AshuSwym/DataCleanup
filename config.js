4f51166375c0ecb87e012a8312495da8a5d93d29
AshuSwym <ashutosh.singh@swymcorp.com>
2024-09-10 15:04:28 +0530

    Parsing TTL

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ dotenv.config();
 
 module.exports = {
 	mongoConnectionURI: process.env.MONGO_CONN_URI,
-	TTL: process.env.TTL,
+	TTL: parseInt(process.env.TTL),
 	INTERVAL: parseInt(process.env.INTERVAL),
 };
